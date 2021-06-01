ef0874b30668f900fd3b8fd068c994cc7c800bfd
Patyoo <patrik.podhorsky@gmail.com>
2021-06-01 15:17:17 +0200

    added cors,routing for BE, styles for Chat and Join

--- client/src/components/Join.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Join = () => {
-  return <h1>Joins</h1>;
-};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const router = require('./router');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('We have a connection');
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+  });
+  socket.on('disconnect', () => {
+    console.log('User left');
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log('Server started'));
