27641622335c4d2a12888b27e032bb0eccb2a053
Patyoo <patrik.podhorsky@gmail.com>
2021-06-02 09:53:35 +0200

    added socketIo functions and custom functions for handling users actions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const router = require('./router');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,10 +15,30 @@ const io = socketio(server, {
 });
 
 io.on('connection', (socket) => {
-  console.log('We have a connection');
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name} has joined` });
+    socket.join(user.room);
+    callback();
   });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.io);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+    callback();
+  });
+
   socket.on('disconnect', () => {
     console.log('User left');
   });
